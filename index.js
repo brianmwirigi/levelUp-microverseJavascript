f755dca80ad17f114a97942fbafb813925159135
brianmwirigi <bmmaingi17@gmail.com>
2023-03-10 02:36:51 +0300

    javascript

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-//variables
-let username = 'mwirigi';
-console.log("hello", username, "how are you?");
+//variables 
+let myname = 'Brian';
+console.log("Hello", myname, "how are you?");
 
 let totalapples = 20;
 let totalusers = 5;
@@ -8,3 +8,21 @@ let totalusers = 5;
 let applesperuser = totalapples / totalusers;
 
 console.log("you have", applesperuser, 'apples');
+
+//variable declarations keyword + name then value
+//box with a label and content
+let country = 'Kenya';
+console.log(country); //value of variable is display
+console.log("country"); //string display
+
+//data types that can be stored in a variable
+//string which is a sequence of characters that can be wrapped around using double quotes
+//variables that are numbers
+//combine multiple variables in the consolelog
+//boolean variable that contains either value true or false
+
+let county = 'Nairobi';
+let year = 2023
+let nightime = false;
+
+console.log(county, year, nightime);
