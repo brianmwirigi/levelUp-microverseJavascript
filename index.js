0c1e0ad835fd7467e79c4a63c44ee42d4c975c42
brianmwirigi <bmmaingi17@gmail.com>
2023-03-10 02:25:43 +0300

    javascript

---
+++ index.js
@@ -0,0 +1,10 @@
+//variables
+let username = 'mwirigi';
+console.log("hello", username, "how are you?");
+
+let totalapples = 20;
+let totalusers = 5;
+
+let applesperuser = totalapples / totalusers;
+
+console.log("you have", applesperuser, 'apples');
