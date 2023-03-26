21f5835a83654d9d67d7f0b9b88bb4a84fc44d86
brianmwirigi <bmmaingi17@gmail.com>
2023-03-26 15:02:23 +0300

    javascript

---
+++ level 3/exercise1.js
@@ -0,0 +1,21 @@
+//tweaking loops to count
+//any operation can  be used
+for (let i = 0; i <= 12; i += 3) {
+    console.log(i);
+}
+
+//backwards
+
+for (let i = 12; i >= 0; i -= 3) {
+    console.log(i);
+}
+
+//exponential expression
+for (let i = 1; i < 200; i *= 2) {
+    console.log(i);
+}
+
+//exercise 1 anser
+for (let i = 102; i > 27; i -= 3) {
+    console.log(i);
+}
