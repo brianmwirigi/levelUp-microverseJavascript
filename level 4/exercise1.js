6ca45ed5cac2223a5a530b240197ada90a1ca904
brianmwirigi <bmmaingi17@gmail.com>
2023-05-20 18:58:29 +0300

    javascript challenges level 4

---
+++ level 4/exercise1.js
@@ -0,0 +1,32 @@
+function sumAll(arr) {
+    let first = arr[0];
+    let last = arr[1];
+
+    if (first > last) {
+        first = arr[1];
+        last = arr[0];
+    }
+
+
+    let output = 0;
+    for (i = first; i <= last; i++) {
+        console.log(i);
+        output += i;
+    }
+
+    return output;
+}
+
+let result = sumAll([10, 5]);
+console.log(result);
+
+//convert celsius to fehrenheit
+
+function convertCtoF(celsius) {
+    let fehrenheit = (celsius * (9 / 5) + 32);
+    return fehrenheit;
+
+}
+let result1 = convertCtoF(30);
+
+console.log(result1);
