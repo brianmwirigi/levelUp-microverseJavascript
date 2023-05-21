a501c4836af28007963991b4597d931e1b8ae081
brianmwirigi <bmmaingi17@gmail.com>
2023-05-21 10:09:42 +0300

    javascript challenges level 4

---
+++ level 4/exercise3.js
@@ -0,0 +1,11 @@
+//factorialize a number
+function factorialize(num) {
+    let output = 1;
+    output *= 2;
+    output *= 3;
+    output *= 4;
+    output *= 5;
+    return output;
+}
+
+console.log(factorialize(5));
