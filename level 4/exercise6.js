4dde2247b4e374aa9eae5c7370ad32dd35aefcc3
brianmwirigi <bmmaingi17@gmail.com>
2023-05-22 23:08:55 +0300

    javascript level 4

--- level 4/exercise6.js
+++ level 4/exercise6.js
@@ -1,20 +1,20 @@
 //a function that splits an array into groups the lenght of size and return them as a two-dimensional aaray
-function chunkArrayIngroups(arr, size) {
+function chunkArrayInGroups(arr, size) {
     let output = [];
     let subArray = [];
-    let subcounter = 0;
+    let subCounter = 0;
 
     for (let i = 0; i < arr.length; i++) { //loop through all elements in arr
         //increase subcounter untill we reach size 
         //reset subcounter
-        if (subcounter === size) {
-            subcounter = 0;
+        if (subCounter === size) {
+            subCounter = 0;
             output.push(subArray);
             subArray = []; //empty subarray 
         }
         subArray.push(arr[i]);
-        console.log(subcounter, subArray);
-        subcounter++;
+        console.log(subCounter, subArray);
+        subCounter++;
 
     }
 
@@ -24,4 +24,20 @@ function chunkArrayIngroups(arr, size) {
     return output;
 }
 
-chunkArrayIngroups(["a", "b", "c", "d"], 3);
+chunkArrayInGroups(["a", "b", "c", "d"], 2);
+chunkArrayInGroups([0, 1, 2, 3, 4, 5], 4);
+
+
+
+//solution2
+function chunkArrayInGroups1(arr, size) {
+    const newArr = [];
+    let i = 0;
+    while (i < arr.length) {
+        newArr.push(arr.slice(i, i + size));
+        i += size;
+    }
+    return newArr;
+}
+
+chunkArrayInGroups1([0, 1, 2, 3, 4, 5], 4);
--- level 4/exercise7.js
+++
@@ -1,7 +0,0 @@
-//truncating string
-function truncateString(str, num) {
-    let output = str(0, 8);
-    return str;
-}
-
-truncateString("A brown fox crossed the road", 8);
